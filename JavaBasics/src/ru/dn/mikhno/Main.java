00c5d7e5f1252f5358c2aeba8277ae9b4a774bd7
Notebook <denismikhno@gmail.com>
2023-10-29 22:12:33 +0300

    Вывел на печать "Привет, мир!"

--- JavaBasics/src/ru/dn/mikhno/Main.java
+++ JavaBasics/src/ru/dn/mikhno/Main.java
@@ -1,10 +1,11 @@
 package ru.dn.mikhno;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello, world!");
 	}
 
 }
